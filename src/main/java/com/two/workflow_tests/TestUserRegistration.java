54eda6f884ad0f1d86262d6d535800a90b9264aa
gerryfletch <1997Gerry@gmail.com>
2019-10-16 15:23:52 +0100

    Removed useless constructor

--- src/main/java/com/two/workflow_tests/TestUserRegistration.java
+++ src/main/java/com/two/workflow_tests/TestUserRegistration.java
@@ -4,12 +4,7 @@ import java.time.LocalDate;
 
 import static com.two.http_api.model.PublicApiModel.UserRegistration;
 
-public class TestUserRegistration extends UserRegistration {
-
-    public TestUserRegistration(String email, String password, String name, LocalDate dob) {
-        super(email, password, name, dob);
-    }
-
+public class TestUserRegistration {
     public static UserRegistration validUser() {
         return new UserRegistration(
                 UserUtil.uniqueEmail(),
@@ -18,5 +13,4 @@ public class TestUserRegistration extends UserRegistration {
                 LocalDate.parse("1997-08-21")
         );
     }
-
 }
